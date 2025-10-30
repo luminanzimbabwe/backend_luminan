f0ace919294aa9d21bd32172d30eea7c4198232b
luminanzimbabwe <thisismeprivateisaacngirazi@gmail.com>
2025-10-30 20:21:50 +0200

    Initial commit - ready for Render deployment

--- myproject/settings.py
+++ myproject/settings.py
@@ -150,7 +150,25 @@ CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     "https://backend-luminan.onrender.com",
     "http://localhost:8081",
-    "http://127.0.0.1:8081"
+    "http://127.0.0.1:8081",
+    "http://localhost:8082",
+    "http://127.0.0.1:8082",
+    "http://localhost:8083",
+    "http://127.0.0.1:8083",
+    "http://localhost:8084",
+    "http://127.0.0.1:8084",
+    "http://localhost:8085",
+    "http://127.0.0.1:8085",
+    "http://localhost:8086",
+    "http://127.0.0.1:8086",
+    "http://localhost:8087",
+    "http://127.0.0.1:8087",
+    "http://localhost:8088",
+    "http://127.0.0.1:8088",
+    "http://localhost:8089",
+    "http://127.0.0.1:8089",
+    "http://localhost:8090",
+    "http://127.0.0.1:8090",
 ]
 
 
