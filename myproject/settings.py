c35c3a7f11c9e9f793fc6b56be36b6f01400c4cf
luminanzimbabwe <thisismeprivateisaacngirazi@gmail.com>
2025-10-31 00:37:43 +0200

    Initial commit - ready for Render deployment

--- myproject/settings.py
+++ myproject/settings.py
@@ -184,8 +184,8 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),       
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),         
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=500),       
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=700),         
 }
 
 # If set to True, generated JWTs will omit the 'exp' claim and therefore
