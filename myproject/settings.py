89516bf9e1d5e89b188a2ae7769b7105afc518c6
luminanzimbabwe <thisismeprivateisaacngirazi@gmail.com>
2025-10-30 20:49:36 +0200

    Initial commit - ready for Render deployment

--- myproject/settings.py
+++ myproject/settings.py
@@ -43,30 +43,7 @@ DEBUG = config('DEBUG', cast=bool, default=False)
 
 
 ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default='*').split(',')
-CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8081",
-    "http://127.0.0.1:8081",
-    "http://localhost:8082",
-    "http://127.0.0.1:8082",
-    "http://localhost:8083",
-    "http://127.0.0.1:8083",
-    "http://localhost:8084",
-    "http://127.0.0.1:8084",
-    "http://localhost:8085",
-    "http://127.0.0.1:8085",
-    "http://localhost:8086",
-    "http://127.0.0.1:8086",
-    "http://localhost:8087",
-    "http://127.0.0.1:8087",
-    "http://localhost:8088",
-    "http://127.0.0.1:8088",
-    "http://localhost:8089",
-    "http://127.0.0.1:8089",
-    "http://localhost:8090",
-    "http://127.0.0.1:8090",
-    "https://backend-luminan.onrender.com",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 
@@ -143,6 +120,7 @@ CORS_ALLOW_HEADERS = [
     "user-agent",
     "x-csrftoken",
     "x-requested-with",
+    "x-admin-pin",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
